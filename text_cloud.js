d7a231253afee87a90bdd389d19865789804c77f
abalone0204 <abalone0204@gmail.com>
2015-01-25 21:20:27 +0800

    adjust text cloud

--- text_cloud.js
+++ text_cloud.js
@@ -1,6 +1,128 @@
+var fill = d3.scale.category20(),
+  cloudSampleNum = 25;
+var w = $('.search').width(),
+  h = $('.search').height(),
+  margin = {
+    left: 15,
+    top: 20
+  },
+  center = {
+    x: ~~(w / 2),
+    y: ~~(h / 2)
+  };
 
-  var fill = d3.scale.category20(),cloudSampleNum=25,
-    w = $('.search').width(),
+
+
+// Text setting
+var textStyle = {
+    "font-size": function(d) {
+      return d.size + "px";
+    },
+    "font-family": "Impact",
+    "fill": '#999',
+    'opacity': 0
+  },
+  textAttr = {
+    "text-anchor": 'middle',
+    "transform": function(d, i) {
+      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+    }
+  };
+
+renderCloud();
+
+function draw(words) {
+  var canvas = d3.select("#textCloud")
+    .append("svg").attr({
+      "width": w,
+      "height": h
+    });
+  var text = canvas.append("g")
+    .attr("transform", "translate(" + [center.x, center.y] + ")")
+    .selectAll("text")
+    .data(words)
+    .enter()
+    .append("text")
+    .style(textStyle);
+
+  text.text(function(d) {
+      return d.text;
+    })
+    .style('opacity', 0);
+  text.transition()
+    .duration(500)
+    .delay(function(d, i) {
+      return i * 10;
+    })
+    .attr(textAttr)
+    .style('opacity', 1);
+  text.transition()
+    .delay(500)
+    .duration(500)
+    .style('fill', '#DDD');
+  // title
+  var titleAttr = {
+      x: center.x,
+      y: center.y,
+      "text-anchor": 'middle',
+      class: "shadow"
+    },
+    titleStyle = {
+      'fill': '#0d7963',
+      'opacity': 0,
+      'font-size': 50,
+    };
+  title = canvas.append('text')
+    .attr(titleAttr)
+    .style(titleStyle)
+    .text("NCCU選課查詢好棒棒");
+  title.transition()
+    .duration(600)
+    .delay(500)
+    .style('opacity', 1);
+  // Set event listenter
+  text.on('click', function() {
+    str = d3.select(this).text();
+    $('#className').val(str);
+  });
+  text.on('mouseover', function() {
+    hideTitle();
+    d3.select(this).transition()
+      .duration(300)
+      .style('fill', 'tomato');
+
+  });
+  text.on('mouseout', function() {
+    showTitle();
+    d3.select(this).transition()
+      .duration(300)
+      .style('fill', '#DDD');
+  });
+  title.on('click', function() {
+    d3.select('svg').remove();
+    renderCloud();
+  });
+  // canvas.on('mouseover', function() {
+  //   title.transition()
+  //     .duration(250)
+  //     .style('opacity', 0);
+  // });
+}
+
+function hideTitle() {
+  title.transition()
+    .duration(250)
+    .style('opacity', 0);
+}
+
+function showTitle() {
+  title.transition()
+    .duration(250)
+    .style('opacity', 1);
+}
+
+function renderCloud() {
+  var w = $('.search').width(),
     h = $('.search').height(),
     margin = {
       left: 15,
@@ -9,119 +131,9 @@
     center = {
       x: ~~(w / 2),
       y: ~~(h / 2)
-    };
-
-
-  // Text setting
-  var textStyle = {
-      "font-size": function(d) {
-        return d.size + "px";
-      },
-      "font-family": "Impact",
-      "fill": '#999',
-      'opacity': 0
     },
-    textAttr = {
-      "text-anchor": 'middle',
-      "transform": function(d, i) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      }
-    };
-
- renderCloud();
-  function draw(words) {
-    var canvas = d3.select("#textCloud")
-      .append("svg").attr({
-        "width": w,
-        "height": h
-      });
-    var text = canvas.append("g")
-      .attr("transform", "translate(" + [center.x, center.y] + ")")
-      .selectAll("text")
-      .data(words)
-      .enter()
-      .append("text")
-      .style(textStyle);
-
-    text.text(function(d) {
-        return d.text;
-      })
-      .style('opacity', 0);
-    text.transition()
-      .duration(500)
-      .delay(function(d, i) {
-        return i * 10;
-      })
-      .attr(textAttr)
-      .style('opacity', 1);
-    text.transition()
-      .delay(500)
-      .duration(500)
-      .style('fill', '#DDD');
-    // title
-    var titleAttr = {
-        x: center.x,
-        y: center.y,
-        "text-anchor": 'middle',
-        class: "shadow"
-      },
-      titleStyle = {
-        'fill': '#0d7963',
-        'opacity': 0,
-        'font-size': 50,
-      };
-    title = canvas.append('text')
-      .attr(titleAttr)
-      .style(titleStyle)
-      .text("NCCU選課查詢好棒棒");
-    title.transition()
-      .duration(600)
-      .delay(500)
-      .style('opacity', 1);
-    // Set event listenter
-    text.on('click', function(){
-      str = d3.select(this).text();
-      $('#className').val(str);
-    });
-    text.on('mouseover', function(){
-      hideTitle();
-      d3.select(this).transition()
-          .duration(300)
-          .style('fill', 'tomato');
-
-    });
-    text.on('mouseout', function(){
-      showTitle();
-      d3.select(this).transition()
-          .duration(300)
-          .style('fill', '#DDD');
-    });
-    title.on('click',function(){
-      d3.select('svg').remove();
-      renderCloud();
-    });
-    // canvas.on('mouseover', function() {
-    //   title.transition()
-    //     .duration(250)
-    //     .style('opacity', 0);
-    // });
-  }
-
-function hideTitle(){
-  title.transition()
-        .duration(250)
-        .style('opacity', 0);
-}
-
-function showTitle(){
-  title.transition()
-        .duration(250)
-        .style('opacity', 1);
-}
-
-function renderCloud(){
-target = _.sample(textCloudWords, cloudSampleNum);
-d3.layout.cloud().size([w, h])
+    target = _.sample(textCloudWords, cloudSampleNum);
+  d3.layout.cloud().size([w, h])
     .words(target.map(function(d) {
       return {
         text: d,
